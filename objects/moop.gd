a7ec9595435804b654919d4003fc1fc76f4561e1
dalifyyy <d4lified@gmail.com>
2026-03-16 23:26:18 +0800

    .

--- objects/moop.gd
+++
@@ -1,8 +0,0 @@
-extends RigidBody3D
-
-enum STATES {IDLE, SEARCHING, DESPERATE, SLEEPING, RUNNING, ALERT}
-var current_state:STATES = STATES.IDLE
-
-func _process(_delta):
-	if current_state == STATES.IDLE:
-		pass
