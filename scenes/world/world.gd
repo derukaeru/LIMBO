017cfd7c26f27a9dcf197f5262473d183d839905
dalifyyy <d4lified@gmail.com>
2026-05-30 20:17:37 +0800

    .

--- scenes/world/world.gd
+++ scenes/world/world.gd
@@ -3,20 +3,20 @@ extends Node3D
 var ui_node
 
 func _ready():
-	ui_node = G.gn("ui")
+	ui_node = Util.gn("ui")
 
 func _process(_delta):
 	ui_node.get_node("datura_count").text = "datura: %s" % [$flowers/datura.get_child_count()]
 	ui_node.get_node("fps").text = "fps: %d" % [Engine.get_frames_per_second()]
-	ui_node.get_node("cull").text = "cull distance: %d" % [G.cull_dist]
+	ui_node.get_node("cull").text = "cull distance: %d" % [Util.cull_dist]
 	
 	if Input.is_action_just_pressed("reduce_cull"):
-		G.cull_dist -= 5
+		Util.cull_dist -= 5
 	elif Input.is_action_just_pressed("increase_cull"):
-		G.cull_dist += 5
+		Util.cull_dist += 5
 
 func generate_flowers():
-	var datura = preload("res://objects/datura.tscn")
+	var datura = load("res://objects/datura.tscn")
 	
 	for i in 10:
 		var d = datura.instantiate()
